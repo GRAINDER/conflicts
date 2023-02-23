49985c27c1a28721a09591c2dbbfcc8ff661a0aa
Paulius Dailidonis <pdailidonis@gmail.com>
2023-02-23 21:04:19 +0200

    calc function commit

--- calc.py
+++ calc.py
@@ -1,21 +1,14 @@
+def add(x: int, y: int) -> int:
+    return x + y
 
-def add(x: int,y: int) -> int:
-     return x + y
- 
-def sub(x: int,y: int) -> int:
-     return x - y
- 
-def prod(x: int,y: int) -> int:
+
+def sub(x: int, y: int) -> int:
+    return x - y
+
+
+def prod(x: int, y: int) -> int:
     return x * y
- 
-def div(x: int,y: int) -> float:
-    return x / y
-
-#if __name__ == "__main__": si eilute rasome tam, kad suprasti, ar pagrindiame kodo lange, leidziam importuoto modulio funckija.
 
 
-print(f'The __name__ from script1 is "{__name__}"')
-if __name__ == "__main__":
-
-     print(f"Name is: {__name__}")
-     
+def div(x: int, y: int) -> int:
+    return x // y
